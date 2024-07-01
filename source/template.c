8c1ea0821e5ff445f593df8c3ea9538ea8a1a92f
jgm0 <jacobgermanamccray@gmail.com>
2024-07-01 17:18:42 -0400

    added DS_Store to gitignore

--- source/template.c
+++ source/template.c
@@ -60,7 +60,7 @@ int main(int argc, char **argv) {
     initializeVideoConfig();
 
 	/*  The console understands VT terminal escape codes.
-	 *  This positions the cursor on row 2, column 0.
+	 *  This positions the cursor on row 2, column 0
 	 *  we can use variables for this with format codes too
 	 *  e.g. printf ("\x1b[%d;%dH", row, column );
 	 */
